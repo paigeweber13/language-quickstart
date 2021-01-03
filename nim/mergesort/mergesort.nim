03545e5fa5156b2b2bde5656689781cfbb113ec5
Riley <rileyw13@protonmail.com>
2021-01-03 13:47:26 -0500

    implemented isSorted

--- nim/mergesort/mergesort.nim
+++ nim/mergesort/mergesort.nim
@@ -5,19 +5,30 @@ proc newRandomArray(len: Natural): seq[float] =
   # results each time
 
   # randomize()
+  const MAX_NUMBER = 100.0
 
   var arr = newSeq[float](len)
   for i in low(arr)..high(arr):
-    arr[i] = rand(100.0)
+    arr[i] = rand(MAX_NUMBER)
 
   return arr
+
+proc isSorted(arr: seq[float]): bool =
+  for i in low(arr)..high(arr) - 1:
+    if arr[i] > arr[i+1]:
+      return false
+
+  return true
 
 # proc merge()
 
 proc main() =
-  var arr = newSeq[float](10)
+  var arr: seq[float] = @[0.0, 1.0, 2.0, 3.0]
   echo arr
+  echo "is arr sorted? ", isSorted(arr)
+
   arr = newRandomArray(10)
   echo arr
+  echo "is arr sorted? ", isSorted(arr)
 
 main()
