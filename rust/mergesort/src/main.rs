b9022a6c54d7c0ad27a1a4071f7963a965527a4d
Riley <rileyw13@protonmail.com>
2021-01-03 19:22:07 -0500

    implemented "array_is_ordered" also moved array initialization into create_random_array function

--- rust/mergesort/src/main.rs
+++ rust/mergesort/src/main.rs
@@ -1,6 +1,7 @@
 use rand::prelude::*;
 
-fn randomize_array(array: &mut Vec<f32>) {
+fn create_random_array(n: usize) -> Vec<f32>{
+    let mut array: Vec<f32> = Vec::with_capacity(n);
     let mut rng = rand::thread_rng();
 
     let max_rand_number = 100.0;
@@ -11,12 +12,24 @@ fn randomize_array(array: &mut Vec<f32>) {
         array.push(this_random_number * max_rand_number);
     }
 
-    ()
+    array
+}
+
+fn array_is_ordered(array: Vec<f32>) -> bool {
+    for i in 0..(array.len() - 1) {
+        if array[i] > array[i+1] {
+            return false;
+        }
+    }
+
+    true
 }
 
 fn main() {
-    let mut array: Vec<f32> = Vec::with_capacity(10);
+    let mut array = create_random_array(10);
     println!("{:?}", array);
-    randomize_array(&mut array);
-    println!("{:?}", array);
+    println!("Array is ordered? {}", array_is_ordered(array));
+    let ordered = vec![1.0, 2.0, 3.0];
+    println!("{:?}", ordered);
+    println!("Array is ordered? {}", array_is_ordered(ordered));
 }
