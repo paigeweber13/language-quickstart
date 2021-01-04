677e4ba1b69f4b4d5b76c08c86db2e9ebb166131
Riley <rileyw13@protonmail.com>
2021-01-03 19:35:47 -0500

    created function stubs just trying to get *something* working before writing mergesort

--- rust/mergesort/src/main.rs
+++ rust/mergesort/src/main.rs
@@ -1,5 +1,30 @@
 use rand::prelude::*;
 
+fn main() {
+    let mut array = create_random_array(10);
+    println!("{:?}", array);
+    println!("Array is ordered? {}", array_is_ordered(array));
+    let ordered = vec![1.0, 2.0, 3.0];
+    println!("{:?}", ordered);
+    println!("Array is ordered? {}", array_is_ordered(ordered));
+
+    // doesn't work?
+    // println!("{:?}", merge(array, ordered));
+}
+
+/* ----------- mergesort ----------- */
+fn mergesort(array: Vec<f32>) -> Vec<f32> {
+    Vec::new()
+}
+
+fn merge(mut left: Vec<f32>, mut right: Vec<f32>) -> Vec<f32> {
+    let mut combined: Vec<f32> = Vec::with_capacity(left.len() + right.len());
+    combined.append(&mut left);
+    combined.append(&mut right);
+    combined
+}
+
+/* ----------- utils ----------- */
 fn create_random_array(n: usize) -> Vec<f32>{
     let mut array: Vec<f32> = Vec::with_capacity(n);
     let mut rng = rand::thread_rng();
@@ -23,13 +48,4 @@ fn array_is_ordered(array: Vec<f32>) -> bool {
     }
 
     true
-}
-
-fn main() {
-    let mut array = create_random_array(10);
-    println!("{:?}", array);
-    println!("Array is ordered? {}", array_is_ordered(array));
-    let ordered = vec![1.0, 2.0, 3.0];
-    println!("{:?}", ordered);
-    println!("Array is ordered? {}", array_is_ordered(ordered));
 }
